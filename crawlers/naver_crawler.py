6ebb446285dc7789188d709ab45890ecdfa955e2
wjlee-ling <wjlee.ling@gmail.com>
2023-01-11 22:57:17 +0900

    [Add] distingushi b/w news and entertian for parsing

--- crawlers/naver_crawler.py
+++ crawlers/naver_crawler.py
@@ -7,9 +7,6 @@ import requests as req
 from bs4 import BeautifulSoup
 from tqdm import trange
 
-# import pytz
-# import datetime
-
 
 class NaverCrawler:
     def __init__(self, headers: str, runtime: str):
@@ -39,22 +36,11 @@ class NaverCrawler:
         if res.status_code == 200:
             try:
                 soup = BeautifulSoup(res.text, "html.parser")
-                title = soup.select_one("h2", {"class": "end_tit"}).text.strip()
-                body = soup.select_one("div.article_body").text.strip()  # class: article_body
-                written_at = soup.select_one("span > em").text.strip()
-                writer = soup.select_one("p.byline_p > span").text.strip()
-                publisher = soup.select_one("div.press_logo").img["alt"]
-
+                news_type = "entertain" if "entertain" in url else "news"
+                parsed_soup = self.parse_soup(soup, news_type)            
                 time.sleep(random.randrange(3))
-                return {
-                    "title": title,
-                    "body": body,
-                    "written_at": written_at,
-                    "writer": writer,
-                    "publisher": publisher,
-                }  # TO-DO: class or namedtupel for news results
+                return parsed_soup # TO-DO: class or namedtupel for news results
             except:
-                print("*****************************")
                 print(f"failed to parse {url}")
                 return None
         else:
@@ -71,14 +57,41 @@ class NaverCrawler:
         }
         for idx in trange(len(urls)):
             news = self.read_news(urls[idx])
-            if news:
-                print(news)
+            if news is not None:
                 item = {"id": f"naver_{query}_{idx}"}
                 item.update(news)
                 output["data"].append(item)
 
         print(f"Crawled {len(output['data'])} articles from the given query '{query}'")  # TO-DO: change to logger
         self.save(query=query, run_time=self.runtime, data=output)
+
+    def parse_soup(self, soup, type) -> dict:
+        """
+        기사의 type(뉴스 또는 연예뉴스)에 따라 다른 tag을 갖고 있기 때문에 달리 parse
+        """
+        if type == "entertain":
+            title = soup.select_one("h2", {"class": "end_tit"}).text.strip()
+            body = soup.select_one("div.article_body").text.strip()  # class: article_body
+            written_at = soup.select_one("span > em").text.strip()
+            writer = soup.select_one("p.byline_p > span").text.strip()
+            publisher = soup.select_one("div.press_logo").img["alt"]
+
+        elif type == "news": 
+            title = soup.select_one("h2.media_end_head_headline").text.strip()
+            body = soup.select_one("div._article_body > div._article_content").text.strip()  # class: article_body
+            written_at = soup.select_one("span._ARTICLE_DATE_TIME").text.strip()
+            writer = soup.select_one("em.media_end_head_journalist_name").text.strip()
+            publisher = soup.select_one("img.media_end_head_top_logo_img").img["alt"]
+        
+        parsed = {
+            "title": title,
+            "body": body,
+            "written_at": written_at,
+            "writer": writer,
+            "publisher": publisher,
+        }
+
+        return parsed
 
     def save(self, query: str, run_time: str, data: dict) -> None:
         with open(f"{self.save_path}_{query}_{run_time}.pickle", "wb") as f:
