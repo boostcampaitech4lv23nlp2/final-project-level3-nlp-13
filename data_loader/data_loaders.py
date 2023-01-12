1585397e2a7b574d5d83a3d934429f37ae81d73c
root <jjn00712@gmail.com>
2023-01-12 04:37:49 +0000

    [Feat] Add dataloader class for GPT
    - GPT 모델용 dataloader 구현
      - 인자로 토크나이저(tokenizer), 데이터 경로(file_path), 토크나이저 최대 길이(max_len)을 입력하여 선언할 수 있다.
      - 선언한 뒤 인스턴스의 tokenized_datasets로 데이터를 불러올 수 있다.
      - ex) dataset = GPTDataset(tokenizer, 'data.csv', 128)
             print(dataset.tokenized_datasets)

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import pyarrow as pa
+import pyarrow.dataset as ds
 import torch
+from datasets import Dataset, DatasetDict
 
 
 class ChatDataset(torch.utils.data.Dataset):
@@ -19,3 +22,42 @@ class ChatDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         return (self.data["input_ids"][index], self.data["attention_mask"][index], self.data["token_type_ids"][index])
+
+
+class GPTDataset:
+    def __init__(self, tokenizer, file_path, max_len=128):
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.raw_datasets = self.load_data(file_path)
+        self.tokenized_datasets = self.raw_datasets.map(
+            self.tokenize,
+            batched=True,
+            remove_columns=self.raw_datasets["train"].column_names,
+        )
+
+    def load_data(self, file_path):
+        raw_data = pd.read_csv(file_path)
+        raw_datasets = DatasetDict(
+            {
+                "train": Dataset(pa.Table.from_pandas(raw_data)),  # .shuffle().select(range(50000)),
+                # "valid": ds_valid.shuffle().select(range(200)),  # .shuffle().select(range(500))
+            }
+        )
+        return raw_datasets
+
+    def tokenize(self, element):
+        outputs = self.tokenizer(
+            element["Q"],
+            element["A"],
+            truncation=True,
+            padding="max_length",
+            max_length=self.max_len,
+            return_overflowing_tokens=True,
+            return_length=True,
+        )
+
+        input_batch = []
+        for length, input_ids in zip(outputs["length"], outputs["input_ids"]):
+            if length == self.max_len:
+                input_batch.append(input_ids)
+        return {"input_ids": input_batch}
