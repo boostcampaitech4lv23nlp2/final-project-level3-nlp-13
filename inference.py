2ae6ce60211bcb8f512f60ff7a50ea2d3c728242
JEONGAH LEE <65378914+jjeongah@users.noreply.github.com>
2023-01-12 13:51:26 +0900

    [Refactor] config

--- inference.py
+++ inference.py
@@ -1,24 +1,22 @@
+import argparse
 import torch
+from tqdm import tqdm
 from transformers import GPT2LMHeadModel, PreTrainedTokenizerFast
-
-tokenizer = PreTrainedTokenizerFast.from_pretrained(
-    "skt/kogpt2-base-v2", bos_token="</s>", eos_token="</s>", unk_token="<unk>", pad_token="<pad>", mask_token="<mask>"
-)
-model = GPT2LMHeadModel.from_pretrained("skt/kogpt2-base-v2")
-model.to("cuda")
-
-
-def encoding(text):
-    text = "<s>" + text + "</s><s>"
-    return torch.tensor(tokenizer.encode(text)).unsqueeze(0).to("cuda")
-
-
-def decoding(ids):
-    return tokenizer.decode(ids)
+import random
+import numpy as np
+import pandas as pd
+from data_loader.data_loaders import ChatDataset
+from tokenizers import SentencePieceBPETokenizer
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from omegaconf import OmegaConf
+import os
 
 
 def get_answer(input_sent):
-    input_ids = encoding(input_sent)
+    # encoding
+    text = "<s>" + input_sent + "</s><s>"
+    input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0).to("cuda")
 
     e_s = tokenizer.eos_token_id
     unk = tokenizer.unk_token_id
@@ -37,9 +35,39 @@ def get_answer(input_sent):
 
     decoded_result = []
     for sample in sample_outputs:
-        decoded_result.append(decoding(sample))
+        decoded_result.append(tokenizer.decode(sample)) # decoding
     for result in decoded_result:
         print(result)
 
+def main(config):
+    print("🔥 get model...")
+    global tokenizer, model
+    tokenizer = PreTrainedTokenizerFast.from_pretrained(
+    config.model.name, bos_token="</s>", eos_token="</s>", unk_token="<unk>", pad_token="<pad>", mask_token="<mask>"
+)
+    model = GPT2LMHeadModel.from_pretrained(config.model.name)
+    model.to("cuda")
+    print("🔥 get input...")
+    get_answer("안녕?")
 
-get_answer("안녕?")
+
+if __name__ == "__main__":
+    # config 설정
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="base_config")
+
+    args, _ = parser.parse_known_args()
+    config = OmegaConf.load(f"./config/{args.config}.yaml")
+
+    # seed 설정
+    SEED = 123
+    random.seed(SEED)
+    np.random.seed(SEED)
+    os.environ["PYTHONHASHSEED"] = str(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.cuda.manual_seed_all(SEED)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    main(config)
