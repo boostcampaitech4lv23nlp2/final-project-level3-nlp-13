9ce8672c5ad5729437b71f75b03940eb5a330237
wjlee-ling <wjlee.ling@gmail.com>
2023-01-12 22:05:28 +0900

    [Fix] parsing error caused by url redirection

--- crawlers/naver_crawler.py
+++ crawlers/naver_crawler.py
@@ -20,12 +20,12 @@ class NaverCrawler:
         self._headers = {
             "X-Naver-Client-Id": NAVER_CLIENT_ID,
             "X-Naver-Client-Secret": NAVER_CLIENT_SECRET,
-            # "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
         }
 
         self.save_path = "data/raw_data/naver"
         self.runtime = runtime
-        self.driver = webdriver.Chrome("/opt/ml/chromedriver", webdriver.ChromeOptions())
+        self.driver = webdriver.Chrome("./chromedriver")
 
     def get_news_urls(self, query="bts", start=1, display=100) -> List:
         """
@@ -40,19 +40,18 @@ class NaverCrawler:
             print(f"Got {len(urls)} urls from Naver.")
 
         else:
-            raise Exception(f"Response failed. Code: {res.status_code}")  # TO-DO: use logger
+            raise Exception(f"Failed to get response from Naver API. Code: {res.status_code}")  # TO-DO: use logger
 
         return urls
 
     def read_news(self, url: str) -> dict:
         try:
             self.driver.get(url)
-            news_type = "entertain" if "entertain" in url else "news"
-            parsed = self.parse(news_type)            
+            self.driver.implicitly_wait(10)
+            parsed = self.parse()            
             time.sleep(3.2)
             return parsed # TO-DO: class or namedtupel for news results
         except:
-            print(f"failed to parse {url}")
             return None
 
     def __call__(self, query, n) -> None:
@@ -67,41 +66,43 @@ class NaverCrawler:
         for idx in trange(len(urls)):
             url = urls[idx]
             news = self.read_news(url)
-            if news is not None:
+            if isinstance(news, dict):
                 item = {"id": f"naver_{query}_{idx}", "url": url}
                 item.update(news)
                 output["data"].append(item)
 
+        self.driver.quit()
         print(f"Crawled {len(output['data'])} articles from the given query '{query}'")  # TO-DO: change to logger
         self.save(query=query, run_time=self.runtime, data=output)
 
-    def parse(self, type) -> dict:
+    def parse(self) -> dict:
         """
-        기사의 type(뉴스 또는 연예뉴스)에 따라 다른 tag을 갖고 있기 때문에 달리 parse
+        기사(뉴스 또는 연예뉴스)에 따라 다른 tag을 갖고 있기 때문에 달리 parse
         """
         by = By.CSS_SELECTOR
-        if type == "entertain":
+        try:
+            # entertain news
             title = self.driver.find_element(by, "h2.end_tit").text.strip()
             body = self.driver.find_element(by, "div.article_body").text.strip()  # class: article_body
-            img_captions = [cap.text.strip() for cap in self.driver.find_elements(by, "em.img_desc")]
-            written_at = self.driver.find_element(by, "span > em").text.strip()
-            writer = self.driver.find_element(by, "p.byline_p > span").text.strip()
-            # publisher = self.driver.find_element(by, "div.press_logo").img["alt"]
+            try:
+                img_captions = [cap.text.strip() for cap in self.driver.find_elements(by, "em.img_desc")]
+            except: 
+                img_captions = []
+            # written_at = self.driver.find_element(by, "span > em").text.strip()
 
-        elif type == "news":
+        except:
+            # news
             title = self.driver.find_element(by, "h2.media_end_head_headline").text.strip()
             body = self.driver.find_element(by, "div._article_content").text.strip()
-            img_captions = [cap.text.strip() for cap in self.driver.find_elements(by, "em.img_desc")]
-            written_at = self.driver.find_element(by, "span._ARTICLE_DATE_TIME").text.strip()
-            writer = self.driver.find_element(by, "em.media_end_head_journalist_name").text.strip()
-            # publisher = self.driver.find_element(by, "img.media_end_head_top_log_img").get_attribute("alt") # 안됨
+            try:
+                img_captions = [cap.text.strip() for cap in self.driver.find_elements(by, "em.img_desc")]
+            except:
+                img_captions = []
 
         body = self.remove_caption(body, img_captions)
         parsed = {
             "title": title,
             "body": body,
-            "written_at": written_at,
-            "writer": writer,
         }
         return parsed
 
@@ -115,6 +116,7 @@ class NaverCrawler:
         return text
 
     def save(self, query: str, run_time: str, data: dict) -> None:
-        with open(f"{self.save_path}_{query}_{run_time}.pickle", "wb") as f:
+        size = len(data["data"])
+        with open(f"{self.save_path}_{query}_{run_time}_size_{size}.pickle", "wb") as f:
             pickle.dump(data, f)
             print(f"Saved to {self.save_path}")
