c38cd3a1b91d83f74b5cdd0360b7f403cfdad746
kimbyeolhee <qufgml0216@naver.com>
2023-01-17 02:59:09 +0000

    [Add] sidebar args 받아오는 부분 추가

--- prototype/app/main.py
+++ prototype/app/main.py
@@ -1,11 +1,7 @@
-from datetime import datetime
-from typing import Any, Dict, List, Optional, Union
-from uuid import UUID, uuid4
-
 from app.model import Chatbot_utils, get_model
-from fastapi import FastAPI, File, Query, UploadFile
+from fastapi import FastAPI
 from fastapi.param_functions import Depends
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from transformers import GPT2LMHeadModel
 
 app = FastAPI()
@@ -13,16 +9,19 @@ app = FastAPI()
 
 class User_input(BaseModel):
     sentence: str
+    max_len: int
+    top_k: int
+    top_p: float
 
 
 @app.post("/input", description="주문을 요청합니다")
 async def make_chat(data: User_input, model: GPT2LMHeadModel = Depends(get_model)):
     generator = Chatbot_utils(model=model[0], tokenizer=model[1])
     text = data.dict()["sentence"]
-    inference_result = generator.get_answer(text)
-    # product = InferenceTextProduct(result=inference_result)
-    # products.append(product)
+    max_len = data.dict()["max_len"]
+    top_k = data.dict()["top_k"]
+    top_p = data.dict()["top_p"]
 
-    # new_order = Order(products=products)
-    # texts.append(new_order)
+    inference_result = generator.get_answer(text, max_len, top_k, top_p)
+
     return inference_result
