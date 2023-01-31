496a4f1e3dc9b5f75376f44a65321a8579435ca6
jjeongah <j_eah@naver.com>
2023-01-31 01:30:34 +0000

    [Add] spam and wordcloud files

---
+++ spam_filter/data_loader/data_loaders.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.dataset as ds
+import torch
+from datasets import Dataset, DatasetDict, load_dataset
+
+class ClassificationDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __getitem__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
