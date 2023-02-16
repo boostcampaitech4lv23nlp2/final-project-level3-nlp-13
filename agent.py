8f5b5c68e2c48781c50e66a5b6b7f6c43d45d71f
root <jjn00712@gmail.com>
2023-02-16 13:00:01 +0000

    [Update] Apply black to agent.py

--- agent.py
+++ agent.py
@@ -4,13 +4,12 @@ from datetime import datetime
 from chatbot.generator.util import Generator
 from chatbot.pipeline.data_pipeline import DataPipeline
 from chatbot.retriever.elastic_retriever import ElasticRetriever
-from twitter.tweet_pipeline import TwitterPipeline
-from utils.classes import BotReply, UserTweet
 from database.mongodb import MongoDB
-from spam_filter.spam_filter import SpamFilter
-
 from omegaconf import OmegaConf
 from pytz import timezone
+from spam_filter.spam_filter import SpamFilter
+from twitter.tweet_pipeline import TwitterPipeline
+from utils.classes import BotReply, UserTweet
 
 # fmt: off
 special_tokens = ["BTS", "bts", "RM", "rm", "진", "김석진", "석진", "김남준", "남준", "슈가", "민윤기", "윤기", "제이홉", "정호석", "지민", "박지민", "뷔", "김태형", "태형", "V", "정국", "전정국", "아미", "빅히트", "하이브", "아미", "보라해" ] #TO-Do
@@ -22,7 +21,7 @@ def main(spam_filter, twitter_pipeline, data_pipeline, elastic_retriever, genera
 
     # twitter api에서 메시지 불러오기
     new_tweets = twitter_pipeline.get_mentions()
-    if len(new_tweets) == 0:  
+    if len(new_tweets) == 0:
         # 새 메시지가 없으면
         time.sleep(60.0)
     else:
